21fb95cf3195727affe337377276643e86e56e17
Mrid <mridsole@gmail.com>
2016-03-17 23:39:23 +1100

    fixed tas2part1

--- lab1/task2part1/Sources/main.asm
+++ lab1/task2part1/Sources/main.asm
@@ -21,18 +21,6 @@
 
 ROMStart        EQU             $4000           ; start address of read only memory
 
-; hex codes for choosing which 7 seg display to write to
-SEG_1_ON        EQU             $0E
-SEG_2_ON        EQU             $0D
-SEG_3_ON        EQU             $0B
-SEG_4_ON        EQU             $07
-
-; display codes for the characters 'F I S H'
-SEG_DISP_F      EQU             $71
-SEG_DISP_I      EQU             $06
-SEG_DISP_S      EQU             $6D
-SEG_DISP_H      EQU             $65
-
                 ORG             RAMStart
                 ; data goes here - no data for now
                 
@@ -44,68 +32,17 @@ _Startup:
                 LDS             #RAMEnd+1       ; initialize the stack pointer
                 CLI                             ; enable interrupts
 
-                LDAA            #$FF            ; load 11111111 into reg A
-                STAA            DDRP            ; configure port P as output
-                STAA            DDRB            ; configure port B as output
+                LDAA            #$00            ; load $00 into register A
+                STAA            DDRH            ; configure port H (the DIP switches) as an input
 
-fish_loop:      JSR             write_fish      ; jump to the write_fish subroutine
-                JMP             fish_loop       ; keep running said subroutine
-
-; ********************************************************************************
-; SUBROUTINE: write_fish
-; ARGS: None
-; writes FISH to the four 7 seg displays
-; ********************************************************************************
-write_fish:
-                LDAA            #SEG_1_ON       ; ...
-                STAA            PTP             ; ... select the first display for writing
-                LDAA            #SEG_DISP_F     ; ...
-                STAA            PORTB           ; ... write 'F' to the first 7 seg
-
-                ; wait for a little bit before writing to the next display:
-                JSR             delay_small    
-
-                LDAA            #SEG_2_ON       ; ...
-                STAA            PTP             ; ... select the second display for writing
-                LDAA            #SEG_DISP_I     ; ...
-                STAA            PORTB           ; ... write 'I' to the second 7 seg
-
-                ; wait a little bit
-                JSR             delay_small
+                LDAA            #$FF            ; load $FF into register A
+                STAA            DDRB            ; configure port B (the data bus - controls LEDs) as output
+                STAA            DDRJ            ; configure port J as output
                 
-                LDAA            #SEG_3_ON       ; ...
-                STAA            PTP             ; ... select the third display for writing
-                LDAA            #SEG_DISP_S     ; ...
-                STAA            PORTB           ; ... write 'S' to the third 7 seg
-
-                ; wait a little bit
-                JSR             delay_small
+                LDAA            #$00
+                STAA            PTJ             ; enable the LEDs by writing $00 to port J
                 
-                LDAA            #SEG_4_ON       ; ...
-                STAA            PTP             ; ... select the fourth display for writing
-                LDAA            #SEG_DISP_H     ; ...
-                STAA            PORTB           ; ... write 'H' to the fourth seg
-
-                ; wait a little bit
-                JSR             delay_small
-
-                RTS                             ; return
-                
-                
-
-; ********************************************************************************
-; SUBROUTINE: delay_small
-; ARGS: None
-; waits for some small amount of time - (probably around 0.5 ms)
-; used for leaving one 7 seg display on for a bit before cycling to the next
-; ********************************************************************************
-delay_small:
-                PSHX                            ; push X to stack, in case it's in use
-                LDX             #500            ; load 500 into X, to count down
-                
-delay_small_L:
-                DEX                             ; decrement X each loop iteration
-                BNE             delay_small_L   ; if X isn't zero, keep looping
-                
-                PULX                            ; pop X off the stack
-                RTS                             ; return from the subroutine
+read_write_LEDs:
+                LDAA            PTH           ; read the DIP switches into register A
+                STAA            PORTB
+                BRA             read_write_LEDs
