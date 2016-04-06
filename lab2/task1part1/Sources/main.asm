b6936918ec644e4768ba985d8c33391c54836e3f
Mrid <mridsole@gmail.com>
2016-04-06 11:49:29 +1000

    yer

--- lab2/task1part1/Sources/main.asm
+++ lab2/task1part1/Sources/main.asm
@@ -30,10 +30,8 @@ PERIOD          EQU             32000
 ; variable/data section
                 ORG             RAMStart
 
-str1            FCB             "Bullshit string 1"
-str2            FCB             "second bullshit string"
-tmp             FCB             $00
-TOSEND          FCB             $00
+str1            FCB             "first string"
+str2            FCB             "second thingy"
 
 ; code section
                 ORG             ROMStart
@@ -43,14 +41,22 @@ _Startup:
                 LDS             #RAMEnd+1       ; initialize the stack pointer
                 CLI                             ; enable interrupts
                 
-                ; configure the timer system
-CONFIG          SEI                             ; disable all interrupts
+                ; configure the serial communications interface
+config_sci      SEI                             ; disable all interrupts
+                
+                ; TODO: find appropriate baud rate value
+                MOVB            #$01,SCIOBDL    ; set the baud rate low byte
+                MOVB            #$01,SCIOBH     ; set the baud rate higher bits
+                
+                ; now set word length and wake up
+
+
                 MOVB            #$01,TCTL1      ; set up output to toggle
                 MOVB            #$10,TIOS       ; select channel 4 for output compare
                 MOVB            #$80,TSCR1      ; enable timers
                 MOVB            #$04,TSCR2      ; prescaler div 16
                 BSET            TIE,#$10        ; enable timer interrupt 4
-                CLI
+                CLI                             ; enable interrupts
                 
                 ; enable the LEDs
                 BSR             LED_ENABLE
@@ -59,15 +65,6 @@ CONFIG          SEI                             ; disable all interrupts
 LOOP:           *
 
 OCISR:
-                LDD             TCNT            ; get current count
-                ADDD            #PERIOD         ; add the period
-                STD             TC4             ; reload TOC2
-                LDAA            TOSEND          ; initial value = $00
-                INCA                            ; increment a
-                STAA            TOSEND          ; save into memory
-                STAA            PORTB           ; send A to LEDs
-                MOVB            #$10,TFLG1      ; ...
-                RTI
 
 LED_ENABLE
                 MOVB            #$FF,DDRB
@@ -75,5 +72,33 @@ LED_ENABLE
                 BCLR            PTJ,$02
                 CLR             PORTB
                 RTS
-                
-mainLoop:
+
+; ********************************************************************************
+; SUBROUTINE: delay_1_sec
+; ARGS: None
+; waits exactly one second, using an outer and inner (nested) loop 
+; with predefined constants - see logbook for derivation
+; (also uses a secondary smaller loop for fine tuning)
+; ********************************************************************************
+delay_1_sec:
+                PSHX                            ; push X to the stack, in case the  caller is using
+                PSHY                            ; same thing for Y
+                LDX             #1000           ; load decrement counter (constant C1) in x
+delay_1_sec_L:  
+                LDY             #5998           ; load decrement counter (constant C2) in y
+delay_1_sec_L2: 
+                DEY                             ; decrement Y every inner loop cycle
+                BNE             delay_1_sec_L2  ; if Y isn't 0, branch to the inner loop
+                DEX                             ; decrement X every outer loop cycle
+                BNE             delay_1_sec_L   ; if X isn't 0, branch to the outer loop
+            
+; this gets pretty close, but not quite: so run another small loop to make up for it
+                LDX             #996            ; constant C3
+delay_1_sec_L3: 
+                DEX                             ; decrement X
+                BNE             delay_1_sec_L3  ; if X isn't zero, branch to loop
+                             
+; before returning, pop original x and y for the caller to use off the stack
+                PULY                            ; pop Y first (correct reverse order)
+                PULX                            ; then pop X
+                RTS                             ; return
